c2d7a85e600979c1c44c242bd544f3985be69e5b
André Kalouguine <andre.kalouguine@ens-lyon.fr>
2023-03-30 11:19:01 +0200

    Rolled back some changes that were making file less readable

--- ART/DefaultOptions.py
+++ ART/DefaultOptions.py
@@ -6,22 +6,26 @@ Created on Wed Jan  4 13:54:51 2023
 """
 DefaultAnalysisOptions = {
     "verbose": True,  # print intermediate results and info in the console?
+
     "plot_Render": False,  # render optical elements and rays, and how many rays to render?
     "maxRaysToRender": 150,
+    "OEPointsToRender": 2000,  # Resolution of mirror meshes
+    "OEPointsScale": 0.5,  # Size of spheres representing mirrors. If zero, only mesh will be rendered.
+
     "DrawAiryAndFourier": True,  # Draw Airy spot and Fourier-limited duration in the following plots?
     "plot_SpotDiagram": False,  # produce an interactive spot diagram without color coding the spots?
     "plot_DelaySpotDiagram": False,  # produce an interactive spot diagram with ray delays color coded?
     "plot_IntensitySpotDiagram": False,  # produce an interactive spot diagram with ray intensities color coded?
     "plot_IncidenceSpotDiagram": False,  # produce an interactive spot diagram with ray incidence angles color coded?
+
     "plot_DelayGraph": False,  # produce an interactive spot diagram with delays in 3rd dimension?
     "plot_IntensityGraph": False,  # produce an interactive spot diagram with delays in 3rd dimension and ray intensities color coded?
     "plot_IncidenceGraph": False,  # produce an interactive spot diagram with delays in 3rd dimension and ray incidence angles color coded?
+
     "plot_DelayMirrorProjection": False,  # produce a plot of the ray delays at the detector projected onto the mirror surface?
     "plot_IntensityMirrorProjection": False,  # produce a plot of the ray intensities at the detector projected onto the mirror surface?
     "plot_IncidenceMirrorProjection": False,  # produce a plot of the ray incidence angles at the detector projected onto the mirror surface?
     "save_results": True,  # save the simulation results to disk, to analyse later
-    "OEPointsToRender": 2000,
-    "OEPointsScale": 0.5,
 }
 
 # %%
